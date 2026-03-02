51b71c1e0122ec08527b082ff8a9d467bf324fc4
dunamis88 <dunamis8@gmail.com>
2026-03-01 23:50:52 -0300

    Agregar icono local para instalacion PWA perfecta

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'profeges-cache-v2';
+const CACHE_NAME = 'profeges-cache-v3';
 const urlsToCache = [
     './',
     './index.html',
     './style.css',
     './app.js',
+    './icon.png',
     'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
     'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'
 ];
