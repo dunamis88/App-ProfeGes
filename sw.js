afcfa13e76c89aa3a501ce36e062cbe8e19395cb
dunamis88 <dunamis8@gmail.com>
2026-03-08 18:50:20 -0300

    Fix cache issues and bump SW

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'profeges-cache-v4';
+const CACHE_NAME = 'profeges-cache-v5';
 const urlsToCache = [
     './',
     './index.html',
     './style.css',
-    './app.js',
+    './app.js?v=5',
     './icon.png',
     'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
     'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css',
