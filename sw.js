a5782e6671718990c37c8a185d8828f7e5b61891
dunamis88 <dunamis8@gmail.com>
2026-03-08 20:23:10 -0300

    Permitir que las tarjetas de eventos y tareas se alarguen automáticamente según su contenido sin scroll

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'profeges-cache-v14';
+const CACHE_NAME = 'profeges-cache-v15';
 const urlsToCache = [
     './',
     './index.html',
-    './style.css',
-    './app.js?v=14',
+    './style.css?v=15',
+    './app.js?v=15',
     './icon.png',
     'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
     'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css',
