aec449e4b37f2e0e6a7190ca0a70edd2382bc6fe
dunamis88 <dunamis8@gmail.com>
2026-03-12 22:36:50 -0300

    Actualización v22: Fondo gris oscuro para la parte trasera de las tarjetas (observaciones)

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'profeges-cache-v21';
+const CACHE_NAME = 'profeges-cache-v22';
 const urlsToCache = [
     './',
     './index.html',
-    './style.css?v=21',
-    './app.js?v=21',
+    './style.css?v=22',
+    './app.js?v=22',
     './icon.png',
     'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
     'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css',
